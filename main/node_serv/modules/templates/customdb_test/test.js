972847f3dea14a5d144763762d25a7a2e1f6e441
havocthehobbit  <havocthehobbit@gmail.com>
2023-10-11 23:58:19 +0200

    auth template

--- main/node_serv/modules/templates/customdb_test/test.js
+++ main/node_serv/modules/templates/customdb_test/test.js
@@ -14,7 +14,40 @@ var main={
             amendUsers : ()=>{
 
             }
-         }
+         },
+         loginAuth : { 
+            checkCanPass : (franeworkData, params, cbp)=>{
+                // passowrd auth override --?  ret.login_confirmed=true/false to login
+                // args --> params : { bd , data , ret_data}
+                let db
+                let temp=""
+                let details={}
+                let view=undefined  
+                
+                if (franeworkData.generalDbFns){
+                    db=franeworkData.generalDbFns.db
+                }else{ // no custom dbs
+                    cbp=params
+                    params=franeworkData
+                }            
+    
+                let cb=()=>{}
+                if (typeof(cbp)==="function"){
+                    cb=cbp
+                }            
+                
+                let ret={
+                    login_confirmed : false,            
+                }                            
+                
+                // customisable logic 
+                if (params.data.password===params.bd.password){
+                    ret.login_confirmed=true;                
+                }
+    
+                cb(ret)
+            }
+        },
     },
     schemas : {
         users4 : {
