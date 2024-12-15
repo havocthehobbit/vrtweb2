7ce80f153f6deb1e8fedba8dd93568c4120a9c0b
havocthehobbit  <havocthehobbit@gmail.com>
2024-12-15 23:54:55 +0200

    update to react 19

--- main/web_ui/src/App.js
+++ main/web_ui/src/App.js
@@ -1,12 +1,12 @@
-import React from "react"
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 import { Main } from './components/views/main'
 
-function App() {  
-  return (   
-      <div className="App">              
-        <Main/>
-      </div>
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
   );
 }
 
